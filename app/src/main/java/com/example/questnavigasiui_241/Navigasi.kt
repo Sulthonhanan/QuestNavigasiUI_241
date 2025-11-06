f9db901dd806c34dcfde5cbebc9c356a111a869d
Sulthonhanan <hananakhnaf31@gmail.com>
2025-11-06 09:33:17 +0700

    fitur(navigasi): implementasi awal navigasi Formulir dan Detail di DetaApp

--- app/src/main/java/com/example/questnavigasiui_241/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_241/Navigasi.kt
@@ -1,8 +1,10 @@
 package com.example.questnavigasiui_241
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import java.lang.reflect.Modifier
 
@@ -19,5 +21,20 @@ fun DetaApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { IsiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
 
+            modifier = Modifier.padding(padding.values = IsiRuang)
+
+
+
+
+
+
+
+        )
+
+    }
 }
